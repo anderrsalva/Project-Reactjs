dadc4d1a9af705faffeab00bc4a9879b66029cfe
Anderson Salvatierra <anderr.salvatierra@gmail.com>
2023-03-07 23:01:57 -0300

    Trabajando en el banner y portada el sitio

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './Banner.scss'
+// import banner from './portadaNordic.jpg'
 
 export const Banner = () => {
   return (
-    <div className='banner__container'>
-        <div className='capa'>
-            <div className='contenido'>
-                <h1>Nordic</h1>
-                <p>ESTÉTICA VEHÍCULAR</p>
+    <div className="banner__portada">
+         {/* <img src={banner} alt="banner"/> */}
+            <div className="capa">
+                    <div className="contenido">
+                        <h1>ESTÉTICA VEHÍCULAR</h1>
+                        <p>Trabajamos sobre el interior y exterior de tu vehículo, además ofrecemos productos de alta calidad para llevarlo a su mejor versión.</p>
+                        <a href='#' className="button">Ubicación</a>
+                        <a href='#' className="button">Contacto</a>
+                    </div>
             </div>
-        </div>
     </div>
   )
 }
