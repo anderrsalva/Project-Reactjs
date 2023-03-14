731bd68130dab9530d234aefeea17e4687ab98cb
Anderson Salvatierra <anderr.salvatierra@gmail.com>
2023-03-14 13:59:47 -0300

    Agregando detalles en cuanto a contenido y estilos

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -11,42 +11,42 @@ export const Servicios = () => {
             <img src="img/servicios/Tezza-1122.jpg" alt="Card Image" />
             <div class="card-body">
               <h5 class="card-title">Tratamiento interior</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente.</p>
             </div>
           </div>
           <div class="card">
             <img src="img/servicios/brillantado.jpg" alt="Card Image" />
             <div class="card-body">
               <h5 class="card-title">Preparación Pre Venta</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente.</p>
             </div>
           </div>
           <div class="card">
             <img src="img/servicios/Tezza-2136.jpg" alt="Card Image" />
             <div class="card-body">
               <h5 class="card-title">Limpieza de motor</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente. </p>
             </div>
           </div>
           <div class="card">
             <img src="img/servicios/Tezza-4274.jpg" alt="Card Image"/>
             <div class="card-body">
               <h5 class="card-title">Tratamientos acrílicos</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente.</p>
           </div>
         </div>
         <div class="card">
             <img src="img/servicios/optica3.png" alt="Card Image"/>
             <div class="card-body">
               <h5 class="card-title">Pintura</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente.</p>
           </div>
         </div>
         <div class="card">
             <img src="img/servicios/Tezza-2136.jpg" alt="Card Image"/>
             <div class="card-body">
               <h5 class="card-title">Correción de laca</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Nuestro equipo de expertos utiliza los mejores productos y técnicas para dejar tu vehículo reluciente.</p>
           </div>
         </div>
       </div>
